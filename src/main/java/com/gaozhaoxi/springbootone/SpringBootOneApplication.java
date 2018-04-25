f6fd21cf180db303fa1dbad22beb143ba31b8eb4
LeonhardtSD <leonhardtsd@163.com>
2018-04-25 16:59:01 +0800

    第二次更新

--- src/main/java/com/gaozhaoxi/springbootone/SpringBootOneApplication.java
+++ src/main/java/com/gaozhaoxi/springbootone/SpringBootOneApplication.java
@@ -1,5 +1,8 @@
 package com.gaozhaoxi.springbootone;
 
+import com.gaozhaoxi.springbootone.bean.Book;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,6 +13,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @SpringBootApplication
 public class SpringBootOneApplication {
+	@Value(value="${book.author}")
+	private String author;
+	@Value(value="${book.name}")
+	private String name;
+	@Value(value="${book.pinyin}")
+	private String pinyin;
+
+
+
+	//自动装配
+	@Autowired
+	private Book book;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootOneApplication.class, args);
@@ -26,10 +41,21 @@ public class SpringBootOneApplication {
 		builder.bannerMode(Banner.Mode.OFF).run(args);
 		 */
 	}
+
 	@RequestMapping(value="/hello",produces = "text/plain;charset=UTF-8")
 	String index(){
-		return "Hello Spring Boot!";
+		return "Hello Spring Boot!"+"\n"
+				+"Hello Spring Boot! The BookName is "+name+";and Book Author is"+author
+				+";and Book pinyin is "+pinyin;
 	}
+
+	@RequestMapping(value="/book")
+	public String book(){
+		return "Hello Spring Boot!"+"\n"
+				+"Hello Spring Boot! The BookName is "+book.getName()+";and Book Author is"+book.getAuthor()
+				+";and Book price is "+book.getPrice();
+	}
+
 
 
 }
